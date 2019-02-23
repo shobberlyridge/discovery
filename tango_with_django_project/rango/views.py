85da92ea6e48e1d63260efb7cb4f510453b0bf89
Edward Parker <edward@albany.me.uk>
2019-02-23 12:06:04 +0000

    well?

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!, after git")
