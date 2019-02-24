920a3b5d93ebf5d837f5ef4f94c4f59890b342c5
Edward Parker <edward@albany.me.uk>
2019-02-24 14:24:31 +0000

    On to 5

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -14,4 +14,6 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse("About - Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+	#return HttpResponse("About - Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+	context_dict = {'aboutmessage': "This is the About message!"}
+	return render(request, 'rango/about.html', context=context_dict)
