6eb6a1fc74b6d53bede5d33bc7a200b641c8c9d3
Edward Parker <edward@albany.me.uk>
2019-03-30 11:46:32 +0000

    Onto Bootstrap

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -9,6 +9,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
 
 # def index(request):
 #    return HttpResponse("Index - Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
@@ -23,6 +25,7 @@ from django.contrib.auth import logout
 #	return render(request, 'rango/index.html', context=context_dict)
 	
 def index(request):
+	request.session.set_test_cookie()
 	# Query the database for a list of ALL categories currently stored.
 	# Order the categories by no. likes in descending order.
 	# Retrieve the top 5 only - or all if less than 5.
@@ -31,10 +34,17 @@ def index(request):
 	category_list = Category.objects.order_by('-likes')[:25]
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
-	# Render the response and send it back!
-	return render(request, 'rango/index.html', context_dict)
-
+	# Obtain our Response object early so we can add cookie information.
+	response = render(request, 'rango/index.html', context_dict)
+	# Call function to handle the cookies
+	visitor_cookie_handler(request, response)
+	# Return response back to the user, updating any cookies that need changed.
+	return response
+	
 def about(request):
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED, by Jove!")
+		request.session.delete_test_cookie()
 	#return HttpResponse("About - Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
 	context_dict = {'aboutmessage': "This is the About message!"}
 	return render(request, 'rango/about.html', context=context_dict)
@@ -234,3 +244,26 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return HttpResponseRedirect(reverse('index'))
+
+
+
+def visitor_cookie_handler(request, response):
+	# Get the number of visits to the site.
+	# We use the COOKIES.get() function to obtain the visits cookie.
+	# If the cookie exists, the value returned is casted to an integer.
+	# If the cookie doesn't exist, then the default value of 1 is used.
+	visits = int(request.COOKIES.get('visits', '1'))
+	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+	'%Y-%m-%d %H:%M:%S')
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		#update the last visit cookie now that we have updated the count
+		response.set_cookie('last_visit', str(datetime.now()))
+	else:
+		visits = 1
+		# set the last visit cookie
+		response.set_cookie('last_visit', last_visit_cookie)
+	# Update/set the visits cookie
+	response.set_cookie('visits', visits)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -131,3 +132,15 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/rango/login/'
+
+# If True, users can register
+REGISTRATION_OPEN = True
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successfully log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+LOGIN_URL = '/accounts/login/'
